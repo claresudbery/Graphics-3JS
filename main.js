fe7a89ec1940347a9638c4ff10bfdc6307bc5fa4
Clare Sudbery <clareworking@claresudbery.co.uk>
2023-06-03 11:55:29 +0100

    make cube bounce up and down

--- main.js
+++ main.js
@@ -14,11 +14,15 @@ var light = new THREE.AmbientLight( 0x404040 ); // soft white light
 scene.add( light );
 var directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 ); scene.add( directionalLight );
 
+let phase = 0;
 camera.position.z = 5;
 
 function animate() {
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
+    
+    phase += 0.05; 
+    camera.position.y = 2 + Math.sin(phase);
 
 	requestAnimationFrame( animate );
 
